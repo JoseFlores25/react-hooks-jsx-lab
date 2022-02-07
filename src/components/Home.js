555c3083e0084497ecd099cd523908993deac336
Jose <flores.jose2185@gmail.com>
2022-02-07 15:22:18 -0500

    All test pass

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,16 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  const colorH = {
+    color: "firebrick",
+  };
+  return (
+    <div id="home">
+      <h1 style={colorH}>
+        {name} is a Web Developer from {city}
+      </h1>
+    </div>
+  );
 }
 
 export default Home;
